04419643bea585f8b04e3ebec521d8c7cca4aac1
KimmyDavis <kimdhev@gmail.com>
2026-05-01 09:50:09 +0100

    changed auth requirements a little

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -137,7 +137,7 @@ export const auth = betterAuth({
       verified: {
         type: "boolean",
         required: false,
-        defaultValue: false,
+        defaultValue: true,
         input: true,
       },
     },
